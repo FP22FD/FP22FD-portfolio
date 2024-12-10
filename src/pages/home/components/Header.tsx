9a6302b2ad602e331aadd7c367f6ec78230dab64
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-10 12:11:58 +0100

    feat: implement Header component

---
+++ src/pages/home/components/Header.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { MenuMobile } from './MenuMobile'
+import { MenuDesktop } from './MenuDesktop'
+
+function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen((prev) => !prev)
+  }
+
+  return (
+    <header className="fixed left-0 top-0 z-40 w-full bg-neutral-white shadow-sm">
+      <nav className="flex w-full border-b p-5" aria-label="Main navigation">
+        <div className="hidden w-full md:block">
+          <MenuDesktop />
+        </div>
+
+        <div className="w-full md:hidden">
+          <MenuMobile
+            isMobileMenuOpen={isMobileMenuOpen}
+            toggleMobileMenu={toggleMobileMenu}
+          />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
