6c3d9093b0b68caf19454588a68c9b9a9cb788a2
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-10 12:26:53 +0100

    feat: implement Footer component

---
+++ src/pages/home/components/Footer.tsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer
+      className="col-span-12 row-start-3 mt-20"
+      role="contentinfo"
+      aria-label="Footer section"
+    >
+      <div className="flex flex-col items-center bg-primary-blue text-typography-white">
+        <div className="text-body-small flex flex-col items-center p-4 text-center font-medium">
+          <p className="mb-1">
+            &copy; 2024 Fernanda Gomes. All Rights Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
