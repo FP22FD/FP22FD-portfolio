3ea09f21ef00ab294fbfbd52ef19f6e876ea85fd
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-14 13:37:58 +0100

    refactor: main Layout component

--- src/pages/home/components/Layout.tsx
+++ src/pages/home/components/Layout.tsx
@@ -4,9 +4,9 @@ import Footer from './Footer'
 
 function Layout() {
   return (
-    <div className="grid min-h-screen grid-cols-12 grid-rows-[min-content_1fr_min-content]">
+    <div className="bg-primary-lightBlue grid min-h-screen grid-cols-12 grid-rows-[min-content_1fr_min-content]">
       <Header />
-      <main className="col-span-10 col-start-2 mt-40">
+      <main className="bg-primary-blueLigter col-span-10 col-start-2 mt-24 md:mt-32">
         <Outlet />
       </main>
       <Footer />
