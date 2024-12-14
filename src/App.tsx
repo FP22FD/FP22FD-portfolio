44b48b14e9447cd7cdfd2b7f5a9e5d93e55be334
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-14 16:08:28 +0100

    update router for deploy with gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './pages/home/components/Layout'
 import { ErrorPage } from './pages/errorPage/ErrorPage'
 import HomePage from './pages/home/HomePage'
 import PageNotFound from './pages/notFound/NotFound'
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      element: <Layout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: '/',
-          element: <HomePage />,
-        },
-      ],
-    },
-    {
-      path: '*',
-      element: <PageNotFound />,
-    },
-  ])
-
-  return <RouterProvider router={router} />
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="error" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
