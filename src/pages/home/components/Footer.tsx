b083dcac1e4939895b728ea1b1f053d2eae1f585
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-14 14:05:01 +0100

    refactor: update Footer component

--- src/pages/home/components/Footer.tsx
+++ src/pages/home/components/Footer.tsx
@@ -1,14 +1,21 @@
+import { PiHeartStraightFill } from 'react-icons/pi'
+
 function Footer() {
   return (
     <footer
-      className="col-span-12 row-start-3 mt-20"
+      className="col-span-12 row-start-3 mt-4 border-t"
       role="contentinfo"
       aria-label="Footer section"
     >
-      <div className="flex flex-col items-center bg-primary-blue text-typography-white">
-        <div className="text-body-small flex flex-col items-center p-4 text-center font-medium">
-          <p className="mb-1">
-            &copy; 2024 Fernanda Gomes. All Rights Reserved
+      <div className="bg-primary-defaultBlue text-primary-darkBlue flex flex-col items-center">
+        <div className="flex flex-col items-center p-2 text-center text-xs font-medium">
+          <p>
+            Developed and designed with
+            <PiHeartStraightFill
+              className="text-primary-darkBlue mx-2 inline"
+              aria-label="heart icon"
+            />
+            <span>by Fernanda Gomes</span>
           </p>
         </div>
       </div>
