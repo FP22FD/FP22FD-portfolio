bbfbb6d896d7d8624e446b554d851bc8ff549416
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-14 13:53:28 +0100

    feat: update styles, add logo, and modify button label in MenuDesktop component

--- src/pages/home/components/MenuDesktop.tsx
+++ src/pages/home/components/MenuDesktop.tsx
@@ -1,33 +1,48 @@
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import Button from './Button'
+import logo from '../../../assets/images/logo.svg'
+import { handleContactClick, handleScroll } from '../utils/scrollUtils'
 
 export function MenuDesktop() {
   const pageLinks = [
     { label: 'Home', to: '#home' },
-    { label: 'About', to: '#about' },
-    { label: 'Skills', to: '#skills' },
     { label: 'Projects', to: '#projects' },
   ]
 
   return (
-    <div className="hidden items-center justify-end space-x-6 md:flex">
-      {pageLinks.map(({ label, to }) => (
-        <NavLink
-          key={label}
-          to={to}
-          className="uppercase text-typography-default hover-underline"
-          aria-label={`Go to ${label}`}
-        >
-          {label}
-        </NavLink>
-      ))}
-      <Button
-        type="button"
-        label="CONTACT-ME"
-        variant="primary"
-        ariaLabel="contact-me"
-        size="large"
-      />
+    <div className="justify-betwenn hidden items-center justify-between md:flex">
+      <div className="text-primary-darkBlue flex">
+        <Link to="#home" aria-label="Go to homepage">
+          <img
+            src={logo}
+            alt="Website logo"
+            className="h-auto w-8 duration-300 hover:rotate-[25deg] 2xl:w-9"
+          />
+        </Link>
+        <h1 className="ml-4 text-xl font-bold 2xl:text-2xl">Fernanda Gomes</h1>
+      </div>
+
+      <div className="hidden items-center space-x-4 md:flex">
+        {pageLinks.map(({ label, to }) => (
+          <NavLink
+            key={label}
+            to={to}
+            className="text-primary-darkBlue uppercase hover-underline"
+            aria-label={`Go to ${label}`}
+            onClick={(e) => handleScroll(e, to.slice(1))}
+          >
+            {label}
+          </NavLink>
+        ))}
+        <Button
+          type="button"
+          label="CONTACT"
+          variant="primary"
+          ariaLabel="contact-me"
+          size="medium"
+          onClick={handleContactClick}
+        />
+      </div>
     </div>
   )
 }
