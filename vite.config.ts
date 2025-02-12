58e76cd982544b5b043d0bcf0b5e8f5101f62299
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-02-12 13:21:09 +0100

    feat: Add ViteImagemin plugin for image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ViteImagemin from 'vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/FP22FD-portfolio/', // this should matches with GitHub repository name
+  plugins: [
+    react(),
+    ViteImagemin({
+      gifsicle: {
+        optimizationLevel: 3,
+        interlaced: true,
+      },
+      optipng: {
+        optimizationLevel: 7, // Max level for PNG optimization
+      },
+      mozjpeg: {
+        quality: 75, // Adjust quality for JPEGs (for future use)
+      },
+      pngquant: {
+        quality: [0.6, 0.8], // Compress PNGs to the specified quality range
+      },
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false,
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/FP22FD-portfolio/', // This match with the GitHub repository name
 });
