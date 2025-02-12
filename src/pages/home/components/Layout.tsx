a0a917ed76a2cdc592700078807b18a20ebbd4bf
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-02-12 13:22:46 +0100

    chore: format code in layout component

--- src/pages/home/components/Layout.tsx
+++ src/pages/home/components/Layout.tsx
@@ -1,17 +1,17 @@
-import { Outlet } from 'react-router-dom'
-import Header from './Header'
-import Footer from './Footer'
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 function Layout() {
   return (
-    <div className="bg-primary-lightBlue grid min-h-screen grid-cols-12 grid-rows-[min-content_1fr_min-content]">
+    <div className="grid min-h-screen grid-cols-12 grid-rows-[min-content_1fr_min-content] bg-primary-lightBlue">
       <Header />
       <main className="bg-primary-blueLigter col-span-10 col-start-2 mt-24 md:mt-32">
         <Outlet />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
