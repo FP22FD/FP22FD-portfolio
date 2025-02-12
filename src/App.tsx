ff3f7f4e7356090e1e5b2fc9b323cce1b45ae6f9
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-02-12 13:23:53 +0100

    update the App router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
+// Lazy load the Layout component
 const Layout = lazy(() => import('./pages/home/components/Layout'));
 const HomePage = lazy(() => import('./pages/home/HomePage'));
 const ErrorPage = lazy(() => import('./pages/errorPage/ErrorPage'));
@@ -9,21 +10,23 @@ const Hotjar = lazy(() => import('./pages/home/components/Hotjar'));
 
 function App() {
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Hotjar />
-
-        <Router>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<HomePage />} />
-            </Route>
+    <Router>
+      <Suspense fallback={<div>Loading Layout...</div>}>
+        <Routes>
+          {/* Wrap Layout with Suspense */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
             <Route path="*" element={<PageNotFound />} />
             <Route path="error" element={<ErrorPage />} />
-          </Routes>
-        </Router>
+          </Route>
+        </Routes>
       </Suspense>
-    </>
+
+      {/* Always render Hotjar in the background */}
+      <Suspense fallback={null}>
+        <Hotjar />
+      </Suspense>
+    </Router>
   );
 }
 
