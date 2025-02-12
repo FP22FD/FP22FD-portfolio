eef898875d0013e19974906abdef5d734b3d4bc6
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-02-13 00:01:10 +0100

    refactor(router): optimize route structure with lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
+import Layout from './pages/home/components/Layout';
+import Hotjar from './pages/home/components/Hotjar';
+import HomePage from './pages/home/HomePage';
+
 // Lazy load the Layout component
-const Layout = lazy(() => import('./pages/home/components/Layout'));
-const HomePage = lazy(() => import('./pages/home/HomePage'));
 const ErrorPage = lazy(() => import('./pages/errorPage/ErrorPage'));
 const PageNotFound = lazy(() => import('./pages/notFound/NotFound'));
-const Hotjar = lazy(() => import('./pages/home/components/Hotjar'));
 
 function App() {
   return (
