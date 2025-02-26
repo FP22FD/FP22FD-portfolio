c49d16a18cd4ae9693e50f039f1d27e1d48deb76
FP22FD <112474910+FP22FD@users.noreply.github.com>
2025-02-26 14:44:22 +0100

    style: refine button component styles

--- src/pages/home/components/Contact.tsx
+++ src/pages/home/components/Contact.tsx
@@ -1,10 +1,10 @@
-import Button from './Button'
+import Button from './Button';
 
 function ContactSection() {
   return (
-    <section className="text-primary-darkBlue mb-8 mt-16 w-full justify-center rounded bg-neutral-ofWhite p-8 text-center shadow-lg">
+    <section className="mb-8 mt-16 w-full justify-center rounded bg-neutral-ofWhite p-8 text-center text-primary-darkBlue shadow-lg">
       <div className="relative mb-6 inline-block">
-        <div className="bg-primary-defaultBlue absolute bottom-0 left-1/2 z-0 h-full w-full -translate-x-20 translate-y-4 transform rounded"></div>
+        <div className="absolute bottom-0 left-1/2 z-0 h-full w-full -translate-x-20 translate-y-4 transform rounded bg-primary-defaultBlue"></div>
         <a href="mailto:fernandapg82@gmail.com">
           <Button
             type="button"
@@ -12,7 +12,7 @@ function ContactSection() {
             variant="primary"
             ariaLabel="contact-me"
             size="large"
-            className="relative z-10 translate-x-3 translate-y-3"
+            className="relative z-10 translate-x-4 translate-y-3 hover:scale-[1.15]"
           />
         </a>
       </div>
@@ -25,7 +25,7 @@ function ContactSection() {
       <p>Email</p>
       <small>fernandapg82@gmail.com</small>
     </section>
-  )
+  );
 }
 
-export default ContactSection
+export default ContactSection;
